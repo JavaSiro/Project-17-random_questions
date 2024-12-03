f95363112666481309faeac9c9d7de8d0d9a3e1d
JavaSiro <javokhirsirojiddinov@gmail.com>
2024-12-03 14:18:44 +0900

    function

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ def load_questions():
         questions = json.load(f)["questions"]
 
     return questions
+
+def get_random_questions(questions, num_questions):
+    if num_questions > len(questions):
+        num_questions = len(questions)
+
+    random_questions = random.sample(questions, num_questions)
+    return random_questions
+
