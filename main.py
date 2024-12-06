764fb225f2c0b182244c864b9e1070cb04160368
JavaSiro <javokhirsirojiddinov@gmail.com>
2024-12-06 19:10:35 +0900

    function

--- main.py
+++ main.py
@@ -16,3 +16,15 @@ def get_random_questions(questions, num_questions):
     random_questions = random.sample(questions, num_questions)
     return random_questions
 
+def ask_question(question):
+    print(question["question"])
+    for i, option in enumerate(question["options"]):
+        print(str(i + 1) + ".", option)
+
+    number = int(input("Select the correct number: "))
+    if number < 1 or number > len(question["options"]):
+        print("Invalid choice, defaulting to wrong answer.")
+        return False
+
+    correct = question["options"][number - 1] == question["answer"]
+    return correct
